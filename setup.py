d3071330b217d5baa36688064adbf6215d9e44a0
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2024-01-04 12:30:04 -0300

    Update package version
    Inclusion of compatibility with problems with imprecise probabilities and the special SysAdmin domain.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name="ippddl-parser",
     description="Planning in Python with Imprecise Probabilities",
-    version="0.2",
+    version="0.3",
     author="André F. Moukarzel",
     url="https://github.com/AndreMoukarzel/ippddl-parser",
     packages=["ippddl_parser"],
